bc745c8f32c83c779a61b2b152b701fa01d8a33b
hyungz <105470827+hyungz@users.noreply.github.com>
2023-06-11 22:28:38 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "hyung_z",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "https://i.imgur.com/FpPNCxD.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "K-Pop Music Video Collector",
     bio: "I collect K-Pop Music Videos",
     email: "hyung_z@mirbudsdrive.com",
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
