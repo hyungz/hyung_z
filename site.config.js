0860a28a0fe925fec73d799763b15aea1627fbea
hyungz <105470827+hyungz@users.noreply.github.com>
2023-06-11 22:42:59 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "hyung_z",
-    image: "https://i.imgur.com/FpPNCxD.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "K-Pop Music Video Collector",
     bio: "I collect K-Pop Music Videos",
     email: "hyung_z@mirbudsdrive.com",
