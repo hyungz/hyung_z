87a41b66c5f58477865cdcf636d7a03bdf61a378
hyungz <105470827+hyungz@users.noreply.github.com>
2023-06-13 20:11:41 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,12 +10,12 @@ const CONFIG = {
     github: "",
     instagram: "",
   },
-  projects: [
-    {
-      name: ``,
-      href: "",
-    },
-  ],
+//  projects: [
+//    {
+//      name: ``,
+//      href: "",
+//    },
+//  ],
   // blog setting (required)
   blog: {
     title: "hyung_z",
