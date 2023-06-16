46935c77e6a321148fc850e4e122cd4b9e15f370
hyungz <105470827+hyungz@users.noreply.github.com>
2023-06-17 02:05:55 +0800

    Update ContactCard.tsx

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -11,7 +11,7 @@ const ContactCard: React.FC = () => {
   return (
     <>
       <div className="p-1 mb-3 dark:text-white">Contact</div>
-      <ul className="rounded-0x0 bg-white dark:bg-zinc-700 p-1 flex flex-col">
+      <ul className="rounded-0.5x0.5 bg-white dark:bg-zinc-700 p-1 flex flex-col">
         {CONFIG.profile.github && (
           <a
             href={`https://github.com/${CONFIG.profile.github}`}
